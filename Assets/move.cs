b5f9a5fd07382a547646913791b91a1c6730f10f
Paul Janicot <janicotpaul@gmail.com>
2024-10-23 11:15:05 +0200

    test

---
+++ Assets/move.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float SpeedX;
+    public float SpeedY;
+    public Vector2 screenSize;
+    public Vector2 flagSize;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flagSize = GetComponent<SpriteRenderer>().bounds.size;
+        screenSize = Camera.main.ViewportToWorldPoint(Vector2.one) - Camera.main.ViewportToWorldPoint(Vector2.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(SpeedX * Time.deltaTime, SpeedY * Time.deltaTime, 0);
+        //if (transform.position.y > (screenSize.y/2)-flagSize)
+        //{
+        //    SpeedY = -SpeedY;
+        //}
+        //if (transform.position.y < (-screenSize.y/2)+flagSize)
+        //{
+        //    SpeedY = -SpeedY;
+        //}
+        //if (transform.position.x > (screenSize.x/2)-flagSize)
+        //{
+        //    SpeedX = -SpeedX;
+        //}
+        //if (transform.position.x < -screenSize.x/2)
+        //{
+        //    SpeedX = -SpeedX;
+        //}
+    }
+}
